925010c3a76a25a76a44a00acaf8090f030f1eed
David Guscic <david.guscic@gmail.com>
2024-05-21 19:00:13 +0200

    Co-authored-by: Diego Banovac <dbanovac@veleri.hr> Co-authored-by: Patrik Močibob <pmocibob@veleri.hr> Co-authored-by: tincar <tincar@users.noreply.github.com> Co-authored-by: tin.car345@gmail.com <tin.car345@gmail.com>

--- projekt.c
+++ projekt.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <malloc.h>
 
+// Struktura podataka
 typedef struct
 {
-    char ime;
-    char liga;
-    char stadion;
-    int kolicina;
+    char ime[50];
+    char liga[50];
+    char stadion[50];
+    int kapacitet;
+    int godina_osnivanja;
+    int broj_naslova;
 } Nogometni_klub;
 
 typedef struct Celija
@@ -16,7 +20,69 @@ typedef struct Celija
     struct Celija *psljedeca;
 } CelijaNogometniKlub;
 
-CelijaNogometniKlub *pronadjiZadnju(CelijaNogometniKlub *ppolaznaCelija);
+// Funkcije
+
+CelijaNogometniKlub *pronadjiZadnju(CelijaNogometniKlub *ppolaznaCelija)
+{
+    CelijaNogometniKlub *pzadnjaCelija = ppolaznaCelija;
+
+    while (pzadnjaCelija->psljedeca != NULL)
+    {
+        pzadnjaCelija = pzadnjaCelija->psljedeca;
+    }
+
+    return pzadnjaCelija;
+}
+// Unos kluba
+void unosKlub(Nogometni_klub *pNogometni_klub)
+{
+    printf("\n Unesite ime kluba: ");
+    scanf_s("%50s", &pNogometni_klub->ime, 50);
+
+    printf("\n Unesite ligu kluba: ");
+    scanf_s("%50s", &pNogometni_klub->liga, 50);
+
+    printf("\n Unesite stadion kluba: ");
+    scanf_s("%50s", &pNogometni_klub->stadion, 50);
+
+    printf("\n Unesite kapacitet stadiona kluba: ");
+    scanf_s("%d", &pNogometni_klub->kapacitet);
+
+    printf("\n Unesite godinu osnivanja kluba: ");
+    scanf_s("%d", &pNogometni_klub->godina_osnivanja);
+
+    printf("\n Unesite broj osvojenih naslova kluba: ");
+    scanf_s("%d", &pNogometni_klub->broj_naslova);
+}
+
+// Ispis kluba
+void ispisKlub(CelijaNogometniKlub *ppolaznaCelija)
+{
+    CelijaNogometniKlub *ptrenutnaCelija = ppolaznaCelija;
+    int brojac = 0;
+
+    printf("\n\n Trenutni klub (adresa kluba: %p)", ptrenutnaCelija);
+    printf("\n\t element: -");
+    printf("\n\t psljedeca %p", ptrenutnaCelija->psljedeca);
+
+    while (ptrenutnaCelija->psljedeca != NULL)
+    {
+        ptrenutnaCelija = ptrenutnaCelija->psljedeca;
+        brojac++;
+
+        printf("\n Ime kluba: %s", ptrenutnaCelija->element.ime);
+
+        printf("\n Liga: %s", ptrenutnaCelija->element.liga);
+
+        printf("\n Stadion: %s", ptrenutnaCelija->element.stadion);
+
+        printf("\n Kapacitete stadiona: %d", ptrenutnaCelija->element.kapacitet);
+
+        printf("\n Godina osnivanja kluba: %d", ptrenutnaCelija->element.godina_osnivanja);
+
+        printf("\n Broj naslova: %d", ptrenutnaCelija->element.broj_naslova);
+    }
+}
 
 void menu()
 {
@@ -30,13 +96,51 @@ void menu()
     printf("\n6: IZLAZ\n\n");
 };
 
+// Izmjena elementa
+void izmjena(Nogometni_klub izmijenjeniElement, CelijaNogometniKlub *pcelijaZaIzmjenu)
+{
+    pcelijaZaIzmjenu->element = izmijenjeniElement;
+}
+
+// Brisanje elementa
+void Brisanje()
+{
+}
+
 int main()
 {
     int odabir;
     Nogometni_klub nogometni_klub;
+    CelijaNogometniKlub *ppolaznaCelija;
 
-    menu();
-    scanf_s("%d", &odabir);
-    printf("%d", odabir);
+    ppolaznaCelija = (CelijaNogometniKlub *)malloc(sizeof(CelijaNogometniKlub));
+    ppolaznaCelija->psljedeca = NULL;
+
+    CelijaNogometniKlub *pzadnjaCelija;
+
+    do
+    {
+        menu();
+        scanf_s("%d", &odabir);
+        switch (odabir)
+        {
+        case 1:
+            unosKlub(&nogometni_klub);
+            break;
+
+        case 2:
+            ispisKlub(&nogometni_klub);
+            break;
+
+        case 3:
+
+        case 4:
+            printf("dasda");
+            break;
+        case 5:
+            break;
+        }
+    } while (odabir < 6);
+
     return 0;
 }
